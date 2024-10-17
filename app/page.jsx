d8cb92974eadb24daee285dfb71978791f3cf23a
YoSirawit <66070314@kmitl.ac.th>
2024-10-17 23:34:51 +0700

    Add demo for retrieving and show data from database

---
+++ app/page.jsx
@@ -0,0 +1,53 @@
+// "use client"
+import React from 'react'
+import Navbar from "./components/Navbar";
+import Link from 'next/link';
+import { sql } from '@vercel/postgres';
+import Petinfo from './components/petinfo';
+import { NextResponse } from 'next/server';
+
+async function getPets(){
+  try{
+    const pets = await fetch('https://testnxt-neon.vercel.app/api/get-pet-info', {cache: 'no-store'});
+    const data = await pets.json();
+    // console.log(data.pets);
+    return data.pets;
+  } catch (error){
+      console.error(error);
+  }
+
+}
+
+function removeall(){
+  sql`DELETE FROM Pets`
+}
+
+async function Home() {
+
+  const pets = await getPets();
+  console.log({pets});
+
+
+  return (
+    <div>
+        <Navbar />
+        <div className='container mx-auto'>
+            <h3>Home</h3>
+            <h1><Link href="/api/get-pet-info">Test</Link></h1>
+            <Link href="/add-pet">Add pet</Link>
+
+        </div>
+            {pets &&
+              pets.map((pet) => {
+                return(
+                  <Petinfo key={pet.name} name={pet.name} owner={pet.owner}/>
+                )})
+
+            }
+    </div>
+  )
+}
+
+
+export default Home
+
