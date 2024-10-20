9fe1d5446535b19541c80d86b2fd5fd6979eee6e
YoSirawit <66070314@kmitl.ac.th>
2024-10-20 17:39:43 +0700

    change match announcement

--- app/api/get-match-announcement/route.js
+++ app/api/get-match-announcement/route.js
@@ -3,11 +3,21 @@ import { NextResponse } from 'next/server';
 import SearchingAnnouncementPage from '../AnnouncementSubsystem/SearchingAnnouncementPage'
  
 export async function GET(request) {
-
+    const { searchParams } = new URL(request.url);
+    // const petName = searchParams.get('petName');
     try{
-        const { rows } = sql
+         const announcement = await SearchingAnnouncementPage.findMyJob();
+         console.log(announcement);
+        //  const announcement = await sqlSELECT * FROM Applicant;
+        //  const data = announcement.json();
+        //  console.log('match-api: ', announcement);
+        // if(pets.rowCount > 0){
+        return NextResponse.json({announcement: announcement })
+        // }else{
+        //     return NextResponse.json({ message : 'notfound' }, {status: 404});
+        // }
     }catch{
         return NextResponse.json({ error }, { status: 500 });
     }
-   
+
 }
